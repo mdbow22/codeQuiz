6f2328c1fc31040f0ecc3756c0f2dbf1a61a7b62
mdbow22 <mdbow22@gmail.com>
2021-08-20 22:25:08 -0400

    created secondary js file with questions for quiz

---
+++ assets/scripts/questions.js
@@ -0,0 +1,16 @@
+let questions = [{
+        question: 'What JS keyword is best practice for initializing a variable?',
+        options: ['let', 'var', 'const', 'butt'],
+        answer: 'let'
+    },
+    {
+        question: 'What keyword allows you to reference the object you are in?',
+        options: ['that', 'there', 'this', 'here'],
+        answer: 'this'
+    },
+    {
+        question: 'Which is NOT a correct way to declare a function?',
+        options: ['function funcName() {}', 'const funcName = function() {}', 'let funcName = function() {}', 'this.function = funcName{}'],
+        answer: 'this.function = funcName{}'
+    }
+]
